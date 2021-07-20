62d729d167026f20f411e4add6735db92219e5f3
Conner Crosby <conner@cavcrosby.tech>
2021-07-19 23:45:15 -0400

    Refactor genconfigs.py
    Several LBYL instances have been replaced with EAFP expressions.
    
    Docstrings have been refactored, w/the extended summary for the
    class docstring being moved into as the module level docstring.
    A lot of the class variables now contain a prefixed underscore. This
    was mainly done to avoid having to write about each class attribute as
    part of the class docstring.
    
    Finally, a script name now does not have to be passed in to flatten
    the configuration datastructure. It is optional todo so if not wanting
    to use the common configuration prefix to denote that a script uses
    a common configuration in the root of the configuration datastructure.

--- python/genconfigs.py
+++ python/genconfigs.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""Docstring for the genconfigs.py program.
+
+This command line tool is used to generate the configurations used by the
+scripts in this repo. The configurations are bare and are expected to be
+modified by the end user to take full advantage of using all the scripts.
+
+The scripts will also use this tool to interface somewhat with the
+datastructure that holds the configurations. I say somewhat, as it just
+flattens the datastructure on demand to try and curb the amount the other
+scripts need to know about the configuration datastructure to use it.
+
+"""
 # Standard Library Imports
 import appdirs
 import argparse
@@ -22,14 +34,6 @@ PROGRAM_ROOT = os.getcwd()
 
 class GenConfigs:
     """Generates/interaces common configurations for scripts in this repo.
-    
-    This command line tool is used to generate the configurations used by the
-    scripts in this repo. The configurations are bare and are expected to be
-    modified by the end user to take full advantage of using all the scripts.
-    The scripts will also use this tool to interface somewhat with the data
-    structure that holds the configurations. I say somewhat, as it just
-    flattens the data structure on demand to try and curb the amount the other
-    scripts need to know about the configuration data structure to use it.
 
     Attributes
     ----------
@@ -37,46 +41,49 @@ class GenConfigs:
         This is the configuration file the program will write/read
         from.
     COMMON_CONFIGURATION_PREFIX : str
-        When used as part of script configuration item key. If the rest of
+        When used as part of a script configuration item key. If the rest of
         the string exists as a key at the root of the configuration tree,
         then the program ignores this configuration item. The idea being
         that I wish to track which scripts use which common configuration(s).
-    
+    default_genconfigs_configs : dict
+        These are the default configurations known to the program and are used
+        to generate the program configuration file.
+
     """
 
     # general configurations
 
-    GPS_CONFIG_FILE = "gps.json"
-    GPS_CONFIG_FILE_DIR_PATH = appdirs.user_config_dir(PROGRAM_NAME)
+    _GPS_CONFIG_FILE = "gps.json"
+    _GPS_CONFIG_FILE_DIR_PATH = appdirs.user_config_dir(PROGRAM_NAME)
     GPS_CONFIG_FILE_PATH = os.path.join(
-        GPS_CONFIG_FILE_DIR_PATH, GPS_CONFIG_FILE
+        _GPS_CONFIG_FILE_DIR_PATH, _GPS_CONFIG_FILE
     )
     COMMON_CONFIGURATION_PREFIX = "_"  # _FOO
 
     # configuration keys
 
-    GITHUB_USERNAME_KEY = "github_username"
-    GITHUB_API_TOKEN_KEY = "github_api_token"
-    SCRIPT_CONFIGS_KEY = "script_configs_keys"
-    WEBHOSTED_GIT_ACCOUNT_URL_KEY = "webhosted_git_account_url"
-    FORKED_REPOS_TO_UPSTREAM_URLS_KEY = "forked_repos_to_upstream_urls"
+    _GITHUB_USERNAME_KEY = "github_username"
+    _GITHUB_API_TOKEN_KEY = "github_api_token"
+    _SCRIPT_CONFIGS_KEY = "script_configs_keys"
+    _WEBHOSTED_GIT_ACCOUNT_URL_KEY = "webhosted_git_account_url"
+    _FORKED_REPOS_TO_UPSTREAM_URLS_KEY = "forked_repos_to_upstream_urls"
 
     # scripts
 
-    UPDATE_REMOTE_FORKS = "update_remote_forks.py"
+    _UPDATE_REMOTE_FORKS = "update_remote_forks.py"
 
     # positional/optional argument labels
     # used at the command line and to reference values of arguments
 
-    EXPORT_SHORT_OPTION = "e"
-    EXPORT_LONG_OPTION = "export"
-    SHOW_PATH_SHORT_OPTION = "s"
-    SHOW_PATH_LONG_OPTION = "show_path"
-    SHOW_PATH_CLI_NAME = SHOW_PATH_LONG_OPTION.replace("_", "-")
+    _EXPORT_SHORT_OPTION = "e"
+    _EXPORT_LONG_OPTION = "export"
+    _SHOW_PATH_SHORT_OPTION = "s"
+    _SHOW_PATH_LONG_OPTION = "show_path"
+    _SHOW_PATH_CLI_NAME = _SHOW_PATH_LONG_OPTION.replace("_", "-")
 
     # positional/optional argument default values
 
-    EXPORT_OPTION_DEFAULT_VALUE = True
+    _EXPORT_OPTION_DEFAULT_VALUE = True
 
     _DESC = (
         "Description: Manages common configurations for the scripts in "
@@ -91,12 +98,12 @@ class GenConfigs:
     )
 
     default_genconfigs_configs = {
-        GITHUB_USERNAME_KEY: "",
-        GITHUB_API_TOKEN_KEY: "",
-        SCRIPT_CONFIGS_KEY: {
-            UPDATE_REMOTE_FORKS: {
-                WEBHOSTED_GIT_ACCOUNT_URL_KEY: "",
-                FORKED_REPOS_TO_UPSTREAM_URLS_KEY: {
+        _GITHUB_USERNAME_KEY: "",
+        _GITHUB_API_TOKEN_KEY: "",
+        _SCRIPT_CONFIGS_KEY: {
+            _UPDATE_REMOTE_FORKS: {
+                _WEBHOSTED_GIT_ACCOUNT_URL_KEY: "",
+                _FORKED_REPOS_TO_UPSTREAM_URLS_KEY: {
                     "awesome-python": "https://github.com/vinta/awesome-python"
                 },
             },
@@ -108,8 +115,8 @@ class GenConfigs:
         self.json = None
 
     @classmethod
-    def _get_calling_program_name(cls, fil):
-        """Determines the calling program's name.
+    def _get_parent_program_name(cls, fil):
+        """Determines the parent program's name.
 
         Parameters
         ----------
@@ -126,7 +133,7 @@ class GenConfigs:
 
         mappings = {"python": 1, "python3": 1}
 
-        if fil != cls.EXPORT_OPTION_DEFAULT_VALUE:
+        if fil != cls._EXPORT_OPTION_DEFAULT_VALUE:
             return fil
         else:
             completed_process = subprocess.run(
@@ -165,10 +172,10 @@ class GenConfigs:
         """
         try:
             cls._arg_parser.add_argument(
-                f"-{cls.EXPORT_SHORT_OPTION}",
-                f"--{cls.EXPORT_LONG_OPTION}",
+                f"-{cls._EXPORT_SHORT_OPTION}",
+                f"--{cls._EXPORT_LONG_OPTION}",
                 nargs="?",
-                const=cls.EXPORT_OPTION_DEFAULT_VALUE,
+                const=cls._EXPORT_OPTION_DEFAULT_VALUE,
                 help=(
                     "creates the intermediate configuration file tailored to "
                     "a script"
@@ -176,8 +183,8 @@ class GenConfigs:
                 metavar="SCRIPT_NAME",
             )
             cls._arg_parser.add_argument(
-                f"-{cls.SHOW_PATH_SHORT_OPTION}",
-                f"--{cls.SHOW_PATH_CLI_NAME}",
+                f"-{cls._SHOW_PATH_SHORT_OPTION}",
+                f"--{cls._SHOW_PATH_CLI_NAME}",
                 action="store_true",
                 help=(
                     "shows the to-be path for the generated "
@@ -191,89 +198,92 @@ class GenConfigs:
             sys.exit(1)
 
     def _flatten_genconfigs_configs(self, script_name):
-        """Minimizes the depth of the configuration datastructure to three levels.
+        """Minimizes the depth of the configuration datastructure loaded.
 
         Script configurations are added to the root node. Which may at most
-        have one more child node. From there, the script configurations tree
+        have two child nodes. From there, the script configurations tree
         is removed.
 
         Parameters
         ----------
         script_name : str
             The script name whose configurations are appended to the
-            configuration datastructure.
+            root of the configuration datastructure.
 
         """
 
-        try:
-            script_configs = self.json[self.SCRIPT_CONFIGS_KEY][script_name]
-            for config in script_configs:
-                if (
-                    self.COMMON_CONFIGURATION_PREFIX in config
-                    and config[
-                        len(
-                            self.COMMON_CONFIGURATION_PREFIX
-                        ) :  # noqa: E203, black adds padding for complex expressions https://github.com/psf/black/issues/446
-                    ]
-                    in self.json
-                ):
-                    continue
-                # Surface the key:value pair, in preparation to flatten the
-                # data structure.
-                self.json[config] = script_configs[config]
-        except KeyError as e:
-            print(
-                f"{PROGRAM_NAME}: "
-                f"no known script configurations for {e.args[0]}",
-                file=sys.stderr,
-            )
-            sys.exit(1)
+        # LBYL ok, as currently I except most scripts will just use common
+        # configurations, with few using the common configuration prefix.
+        script_configs = (
+            self.json[self._SCRIPT_CONFIGS_KEY][script_name]
+            if script_name in self.json[self._SCRIPT_CONFIGS_KEY]
+            else tuple()
+        )
+        for config in script_configs:
+            if (
+                self.COMMON_CONFIGURATION_PREFIX in config
+                and config[
+                    len(
+                        self.COMMON_CONFIGURATION_PREFIX
+                    ) :  # noqa: E203, black adds padding for complex expressions https://github.com/psf/black/issues/446
+                ]
+                in self.json
+            ):
+                continue
+            # Surface the key:value pair, in preparation to flatten the
+            # data structure.
+            self.json[config] = script_configs[config]
 
-        del self.json[self.SCRIPT_CONFIGS_KEY]
+        del self.json[self._SCRIPT_CONFIGS_KEY]
 
-    def _load_json(self):
-        """How json files are loaded for the program.
+    def _load_configs(self):
+        """Used to load the instance with the program configuration file.
 
         Returns
         -------
         bool
-            If the expected json could not be loaded.
+            If the config file could be loaded.
 
         """
-        if not pathlib.Path(self.GPS_CONFIG_FILE_PATH).exists():
+        try:
+            with open(self.GPS_CONFIG_FILE_PATH, "r") as fil:
+                self.json = json.load(fil)
+            return True
+        except FileNotFoundError:
             return False
-        with open(self.GPS_CONFIG_FILE_PATH, "r") as fil:
-            self.json = json.load(fil)
-        return True
 
     def main(self, cmd_args):
         """The main of the program."""
         try:
-            if cmd_args[self.SHOW_PATH_LONG_OPTION]:
+            if cmd_args[self._SHOW_PATH_LONG_OPTION]:
                 print(self.GPS_CONFIG_FILE_PATH)
-            elif cmd_args[self.EXPORT_LONG_OPTION]:
-                if not self._load_json():
+            elif cmd_args[self._EXPORT_LONG_OPTION]:
+                if not self._load_configs():
                     raise FileNotFoundError(1, "_", self.GPS_CONFIG_FILE_PATH)
-                script_name = self._get_calling_program_name(
-                    cmd_args[self.EXPORT_LONG_OPTION]
+                script_name = self._get_parent_program_name(
+                    cmd_args[self._EXPORT_LONG_OPTION]
                 )
                 self._flatten_genconfigs_configs(script_name)
                 print(json.dumps(self.json, indent=4))
             else:
-                if not pathlib.Path(self.GPS_CONFIG_FILE_DIR_PATH).exists():
-                    os.mkdir(self.GPS_CONFIG_FILE_DIR_PATH)
+                pathlib.Path(self._GPS_CONFIG_FILE_DIR_PATH).mkdir(
+                    parents=True, exist_ok=True
+                )
                 with open(self.GPS_CONFIG_FILE_PATH, "w") as fil:
                     json.dump(self.default_genconfigs_configs, fil, indent=4)
             sys.exit(0)
-        except FileNotFoundError as e:
+        except FileNotFoundError as except_obj:
             print(
-                f"{PROGRAM_NAME}: could not find file: {e.filename}",
+                f"{PROGRAM_NAME}: could not find file: {except_obj.filename}",
                 file=sys.stderr,
             )
             sys.exit(1)
-        except Exception as e:
+        except Exception as except_obj:
             traceback.print_exception(
-                type(e), e, e.__traceback__, file=sys.stderr
+                type(except_obj),
+                except_obj,
+                except_obj.__traceback__,
+                file=sys.stderr,
             )
             print(
                 f"{PROGRAM_NAME}: an unknown error occurred, see the above!",
